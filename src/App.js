5a9a61bbc3bd7102221da5706784f46e5321f9a5
jeru27 <jrumora@gmail.com>
2022-03-23 17:37:14 -0700

    done with all I hope it's working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
-
+const projectID ='b2c282c7-daa7-4304-adf5-65e412bfc306';
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
     return (
         <ChatEngine
             height="100vh"
-            projectID="b2c282c7-daa7-4304-adf5-65e412bfc306"
-            userName="jelenar"
-            userSecret="123123"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     )
 }
